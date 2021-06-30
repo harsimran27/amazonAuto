d7d5c3222a26341d7037893734dc9a6aed308ba5
Harsimran singh <simran.9971290436@gmail.com>
2021-06-30 12:27:14 +0530

    adding comments

--- amazonAuto.js
+++ amazonAuto.js
@@ -14,6 +14,8 @@ const jsonFilepath = './amazonData.json';
     const page = await browser.newPage();
     await page.goto('https://www.amazon.com/');
     await page.waitForSelector('.a-cardui-footer a[aria-label="Shop by Category - Shop now"]');
+
+    // go to shop by category and fetch the link 
     let shopByCategoryAnchorLink = await page.evaluate(function () {
         let shopByCategoryAnchor = document.querySelector('.a-cardui-footer a[aria-label="Shop by Category - Shop now"]');
 
@@ -24,6 +26,7 @@ const jsonFilepath = './amazonData.json';
 
     await page.goto(shopByCategoryAnchorLink);
 
+    // go to computer department .fetch the department link and store the link in intermediatedata 
     let intermediateData = await page.evaluate(function () {
         let computerAnchorLink = document.querySelectorAll('.left_nav.browseBox ul li a');
 
@@ -34,10 +37,11 @@ const jsonFilepath = './amazonData.json';
         return { computerLink };
     })
 
+    //create function and send the intermediate data link.
     let computerDetails = await fetchDataForGivenDepartment(intermediateData.computerLink[0]);
 
     for (let i = 0; i < computerDetails.computerItemTypeLinkArr.length; i++) {
-        let itemTypeLink = await goToComputerItemTypeAndFetchItem(computerDetails.computerItemTypeLinkArr[i]);
+        let itemTypeLink = await goToComputerItemTypeAndFetchItem(computerDetails.computerItemTypeLinkArr[i]); //create an array with name of department and store all products in the form of objects . 
         dataObj[computerDetails.computerItemTypeNameArr[i]] = [];
 
         let productType = computerDetails.computerItemTypeNameArr[i];
@@ -50,28 +54,29 @@ const jsonFilepath = './amazonData.json';
                 rating: itemTypeLink.productRatingArr[j],
                 price: itemTypeLink.priceArr[j],
             };
-            dataObj[productType].push(product);
+            dataObj[productType].push(product); // and add all products in the dataObj for a given department.
         }
     }
 
+    //function to get product details for a particular type in computer department.
     function goToComputerItemTypeAndFetchItem(itemLink) {
         return new Promise(function (resolve, reject) {
             page.goto(itemLink).then(function () {
                 return page.evaluate(function () {
                     let productName = document.querySelectorAll(".a-section.a-spacing-medium .a-section.a-spacing-none.a-spacing-top-small h2 a span");
-                    let productNameArr = [];
+                    let productNameArr = []; // add all the products name
                     for (let i = 0; i < productName.length; i++) {
                         productNameArr.push(productName[i].innerText.trim());
                     }
 
                     let productLink = document.querySelectorAll(".a-section.a-spacing-medium .a-section.a-spacing-none.a-spacing-top-small h2 a");
-                    let productLinkArr = [];
+                    let productLinkArr = [];  // add all the product links
                     for (let i = 0; i < productLink.length; i++) {
                         productLinkArr.push("https://www.amazon.com/" + productLink[i].getAttribute('href'));
                     }
 
                     let imgIndex = document.querySelectorAll(".rush-component .a-link-normal.s-no-outline .a-section.aok-relative.s-image-square-aspect img");
-                    let imgIdx = [];
+                    let imgIdx = [];   
                     for (let i = 0; i < imgIndex.length; i++) {
                         imgIdx.push(imgIndex[i].getAttribute("data-image-index"));
                     }
@@ -80,7 +85,7 @@ const jsonFilepath = './amazonData.json';
                     for (let i = 0; i < imgsLink.length; i++) {
                         images.push(imgsLink[i].getAttribute("src"));
                     }
-                    let imageLinkArr = [];
+                    let imageLinkArr = []; //store all image for the products
                     for (let i = 0; i < imgIdx.length; i++) {
                         if (imgIdx[i] != "0") {
                             imageLinkArr.push(images[i]);
@@ -88,7 +93,7 @@ const jsonFilepath = './amazonData.json';
                     }
 
                     let product = document.querySelectorAll(".a-section.a-spacing-medium");
-                    let priceArr = [];
+                    let priceArr = [];   // store price of product .if price not avaliable provide the link to that product.
                     for (let i = 0; i < product.length - 1; i++) {
                         if (product[i].querySelector('.a-offscreen') !== null) {
                             let productPrice = product[i].querySelector(".a-offscreen").innerText;
@@ -99,12 +104,12 @@ const jsonFilepath = './amazonData.json';
                     }
 
                     let productRating = document.querySelectorAll(".a-section.a-spacing-medium ");
-                    let productRatingArr = [];
+                    let productRatingArr = []; // store rating of the given product . if rating not avaliable provide he product link  
                     for (let i = 0; i < productRating.length - 1; i++) {
                         if (productRating[i].querySelector(".a-icon-alt") != null) {
                             productRatingArr.push(productRating[i].querySelector(".a-icon-alt").innerText);
                         } else {
-                            productRatingArr.push("Product rating not avaliable");
+                            productRatingArr.push(`Product rating not avaliable . please visit ${productLinkArr[i]}`);
                         }
                     }
 
@@ -116,14 +121,14 @@ const jsonFilepath = './amazonData.json';
         })
     }
 
-
+//for computer department fetch the relate product link and name.
     function fetchDataForGivenDepartment(Link) {
         return new Promise(function (resolve, reject) {
             page.goto(Link).then(function () {
                 return page.evaluate(function () {
                     let computerItemTypeLink = document.querySelectorAll('.a-spacing-micro.apb-browse-refinements-indent-2 a');
 
-                    let computerItemTypeLinkArr = [];
+                    let computerItemTypeLinkArr = [];  //computer department all links 
 
                     for (let i = 0; i < computerItemTypeLink.length; i++) {
                         computerItemTypeLinkArr.push("https://www.amazon.com/" + computerItemTypeLink[i].getAttribute('href'));
@@ -131,7 +136,7 @@ const jsonFilepath = './amazonData.json';
 
                     let computerItemTypeName = document.querySelectorAll('.a-spacing-micro.apb-browse-refinements-indent-2 span[dir="auto"]');
 
-                    let computerItemTypeNameArr = [];
+                    let computerItemTypeNameArr = []; // computer department  all names. 
 
                     for (let i = 0; i < computerItemTypeName.length; i++) {
                         computerItemTypeNameArr.push(computerItemTypeName[i].innerText.trim());
@@ -146,13 +151,13 @@ const jsonFilepath = './amazonData.json';
         })
     }
 
+    //create json file if not exist to store data in json file.
     jsonExist(jsonFilepath);
 
-    function jsonExist(filepath) {
+    function jsonExist(filepath) {  //function to create json file 
         try {
             if (fs.existsSync(filepath)) {
                 //file exists
-                return;
             } else {
                 let json = JSON.stringify(dataObj);
                 fs.writeFileSync("amazonData.json", json);
